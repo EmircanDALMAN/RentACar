20fed6a9737e04d708826781c2e1e806775d0a32
Kontorta16 <75789956+Kontorta16@users.noreply.github.com>
2021-02-24 15:58:36 +0300

    v1.1

---
+++ ReCapProject.RentACar.WebAPI/Controllers/CarsController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ReCapProject.RentACar.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CarsController : ControllerBase
+    {
+        ICarService 
+    }
+}
