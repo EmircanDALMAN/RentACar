dfc79f252f4073b6d1a54144e2654fc431086e88
Emircan Dalman <emircandalman28@gmail.com>
2021-03-19 18:29:59 +0300

    v1.1

--- Business/Concrete/PaymentManager.cs
+++ Business/Concrete/PaymentManager.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using Business.Abstract;
+﻿using Business.Abstract;
 using Business.ValidationRules.FluentValidation;
 using Core.Aspects.Autofac.Performance;
 using Core.Aspects.Autofac.Transaction;
