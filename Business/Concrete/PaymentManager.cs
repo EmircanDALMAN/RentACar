3b3f81949b7afd4e2e22626b2c8aafac951d6410
Emircan Dalman <emircandalman28@gmail.com>
2021-03-19 18:29:30 +0300

    v1.1

---
+++ Business/Concrete/PaymentManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Business.Abstract;
+using Business.ValidationRules.FluentValidation;
+using Core.Aspects.Autofac.Performance;
+using Core.Aspects.Autofac.Transaction;
+using Core.Aspects.Autofac.Validation;
+using Core.Entities;
+using Core.Utilities.Results;
+
+namespace Business.Concrete
+{
+    public class PaymentManager:  IPaymentService
+    {
+        [ValidationAspect(typeof(FakePaymentValidator))]
+        [PerformanceAspect(5)]
+        [TransactionScopeAspect]
+        public IResult MakePayment(IPaymentModel paymentModel)
+        {
+            return new SuccessResult();
+        }
+    }
+}
