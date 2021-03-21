67b08d78c1d980a3e092d17832e95167dce79e1c
Emircan Dalman <emircandalman28@gmail.com>
2021-03-22 02:16:01 +0300

    v1.1

---
+++ RentACarAngular/RentACarAngular/src/app/components/brand/brand-add/brand-add.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-brand-add',
+  templateUrl: './brand-add.component.html',
+  styleUrls: ['./brand-add.component.css']
+})
+export class BrandAddComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ RentACarAngular/RentACarAngular/src/app/components/car/car-add/car-add.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-car-add',
+  templateUrl: './car-add.component.html',
+  styleUrls: ['./car-add.component.css']
+})
+export class CarAddComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
