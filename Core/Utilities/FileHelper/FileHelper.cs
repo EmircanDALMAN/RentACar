2fb274ba207e1a53191e38081543903904fa7264
Emircan Dalman <emircandalman28@gmail.com>
2021-03-22 16:30:26 +0300

    v1.1

--- Core/Utilities/FileHelper/FileHelper.cs
+++ Core/Utilities/FileHelper/FileHelper.cs
@@ -10,31 +10,28 @@ namespace Core.Utilities.FileHelper
         
         public static string SaveImageFile(string fileName,IFormFile extension)
         {
-            string resimUzantisi = Path.GetExtension(extension.FileName); 
-            string yeniResimAdi = string.Format("{0:D}{1}", Guid.NewGuid(), resimUzantisi);
-            string imageKlasoru = Path.Combine(_wwwRoot, fileName);
-            string tamResimYolu = Path.Combine(imageKlasoru, yeniResimAdi); 
-            string webResimYolu = string.Format("/"+ fileName + "/{0}", yeniResimAdi);
-            if(!Directory.Exists(imageKlasoru))
-                Directory.CreateDirectory(imageKlasoru);
+            string imageExtension = Path.GetExtension(extension.FileName); 
+            string newImageName = string.Format("{0:D}{1}", Guid.NewGuid(), imageExtension);
+            string imageFile = Path.Combine(_wwwRoot, fileName);
+            string fullImagePath = Path.Combine(imageFile, newImageName); 
+            string webImagePath = string.Format("/"+ fileName + "/{0}", newImageName);
+            if(!Directory.Exists(imageFile))
+                Directory.CreateDirectory(imageFile);
 
-            using (var fileStream = File.Create(tamResimYolu))
+            using (var fileStream = File.Create(fullImagePath))
             {
                 extension.CopyTo(fileStream);
                 fileStream.Flush();
             }
-            return webResimYolu;
+            return webImagePath;
         }
 
         public static bool DeleteImageFile(string fileName)
         {
-            string fullPath = Path.Combine(fileName);
-            if (File.Exists(_wwwRoot + fullPath))
-            {
-                File.Delete(_wwwRoot + fullPath);
-                return true;
-            }
-            return false;
+            var fullPath = Path.Combine(fileName);
+            if (!File.Exists(_wwwRoot + fullPath)) return false;
+            File.Delete(_wwwRoot + fullPath);
+            return true;
         }
     }
 }
