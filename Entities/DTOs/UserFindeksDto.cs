18668b1973c8a8fe27eb283b1cff1838cb82a9da
Emircan Dalman <emircandalman28@gmail.com>
2021-03-26 17:06:53 +0300

    v1.1

---
+++ Entities/DTOs/UserFindeksDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.DTOs
+{
+    public class UserFindeksDto
+    {
+        public string TcNo { get; set; }
+        public string DateYear { get; set; }
+        public int UserFindeks { get; set; }
+    }
+}
---
+++ Entities/DTOs/UserForUpdateDto.cs
@@ -0,0 +1,10 @@
+﻿using Core.Entities.Concrete;
+
+namespace Entities.DTOs
+{
+    public class UserForUpdateDto
+    {
+        public User User { get; set; }
+        public string Password { get; set; }
+    }
+}
